ec2e9cd556db8f63b6db0b1d8886af7493664982
dericko681 <abonghoderick@gmail.com>
2025-01-23 15:30:13 +0100

    function: shuffles names from get_names.c
    Signed-off-by: dericko681 <abonghoderick@gmail.com>

---
+++ c_files/shuffle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define NUM_NAMES 18
+#define NAME_LENGTH 50
+
+void shuffle(char names[NUM_NAMES][NAME_LENGTH], int n)
+{
+    for (int i = n - 1; i > 0; i--)
+    {
+        int j = rand() % (i + 1);
+
+        char temp[NAME_LENGTH];
+        strcpy(temp, names[i]);
+        strcpy(names[i], names[j]);
+        strcpy(names[j], temp);
+        printf("%s ", names[i]);
+    }
+
+    // for (int k = 0; k < n; k++)
+    // {
+    //     printf("%s ", names[k]);
+    // }
+}
